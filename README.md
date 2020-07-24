2bc93d992826767d07eabfca19c7795fcab2900f
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2020-07-24 16:33:50 -0300

    Creating screen

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Space-Invaders
+## Space Invaders v1.0 by Thiago Pereira
 
 ```bash
 # Go into the repository
--- source/main.py
+++ source/main.py
@@ -3,8 +3,33 @@ import turtle
 import os
 
 # Defining screen window
-window = turtle.Screen()
-window.bgcolor("black")
-window.title("Space Invaders v1.0 by Thiago Pereira")
+def createWindow() :
+	# Create window
+	window = turtle.Screen()
+	window.bgcolor("black")
+	window.title("Space Invaders v1.0 by Thiago Pereira")
 
-value = input("Press enter to exit")
+	# Draw border
+	border_pen = turtle.Turtle()
+	border_pen.speed(0)
+	border_pen.color("white")
+	border_pen.penup()
+	border_pen.setposition(-270, -270)
+	border_pen.pendown()
+	border_pen.pensize(3)
+
+	for side in range(4) :
+		border_pen.fd(540)
+		border_pen.lt(90)
+
+	border_pen.hideturtle()
+
+	return window
+
+def main() :
+	window = createWindow()
+
+	value = input("Press enter to exit")
+
+if __name__ == "__main__" :
+	main()
